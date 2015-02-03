293884a80b7cbdf8757b4d10e5f07584568ad445
McCall Bliss <missmccallkay@gmail.com>
2015-02-02 20:25:22 -0500

    changed images

--- js/scripts.js
+++ js/scripts.js
@@ -82,6 +82,26 @@ $(".overlay").click(function() {
 	$(".overlay-boxify").toggleClass("open");
 });
 
+/************ Mailing List Transformicon ***************/
+
+/* When user clicks the Icon */
+// $(".nav-toggle").click(function() {
+// 	$(this).toggleClass("active");
+// 	$(".overlay-boxify").toggleClass("open");
+// });
+
+/* When user clicks a link */
+// $(".overlay ul li a").click(function() {
+// 	$(".nav-toggle").toggleClass("active");
+// 	$(".overlay-boxify").toggleClass("open");
+// });
+
+/* When user clicks outside */
+// $(".overlay").click(function() {
+// 	$(".nav-toggle").toggleClass("active");
+// 	$(".overlay-boxify").toggleClass("open");
+// }); 
+
 /***************** Smooth Scrolling ******************/
 
 $('a[href*=#]:not([href=#])').click(function() {
